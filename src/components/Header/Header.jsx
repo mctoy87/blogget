3dd58f0aeb6adc285c4140aac22fc00daac6de84
mctoy87 <mctoy87@mail.ru>
2023-12-15 12:53:20 +0200

    practiced with components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import style from './Header.module.css';
+import Layout from '../Layout';
+
+export const Header = () => {
+  return (
+    <header>
+      <Layout>
+        Привет
+      </Layout>
+    </header>
+    )
+}
