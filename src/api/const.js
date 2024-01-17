b735056dc14e2cd283c263a7a21ff933ce919e96
mctoy87 <mctoy87@mail.ru>
2024-01-17 14:26:22 +0200

    added login functional

--- src/api/const.js
+++ src/api/const.js
@@ -1,4 +1,4 @@
-export const URL = 'https://oauth.reddit.com';
+export const URL_API = 'https://oauth.reddit.com';
 export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'sgRuUgDwvrsPMFOKj_Hl7w';
 export const RESPONSE_TYPE = 'token';
--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,17 +1,46 @@
+import {useEffect, useState} from 'react';
 import style from './Auth.module.css';
 import PropTypes from 'prop-types';
 import {ReactComponent as LoginIcon} from './img/login.svg';
 import {urlAuth} from '../../../api/auth';
 import {Text} from '../../../UI/Text';
+import {URL_API} from '../../../api/const';
 
-// eslint-disable-next-line arrow-body-style
 export const Auth = ({token}) => {
+  const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetch(`${URL_API}/api/v1/me`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(({name, icon_img: iconImg}) => {
+        const img = iconImg.replace(/\?.*$/, '');
+        setAuth({name, img});
+      })
+      .catch((err) => {
+        console.error(err);
+        setAuth({});
+      });
+  }, [token]);
+
   return (
     <div className={style.container}>
-      {token ? (
-        token
+      {auth.name ? (
+        <button className={style.btn}>
+          <img
+            className={style.img}
+            src={auth.img}
+            title={auth.name}
+            alt={`Аватар ${auth.name}`}
+          />
+        </button>
       ) : (
-        <Text As='a' href={urlAuth}>
+        <Text className={style.authLink} As='a' href={urlAuth}>
           <LoginIcon className={style.svg}/>
         </Text>
       )}
