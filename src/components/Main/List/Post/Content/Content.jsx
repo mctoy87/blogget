c7aa70b802228cadd2604c12160d0d2a4656ad76
mctoy87 <mctoy87@mail.ru>
2024-01-26 12:49:21 +0200

    refactoring: created content component for Post

---
+++ src/components/Main/List/Post/Content/Content.jsx
@@ -0,0 +1,34 @@
+import style from './Content.module.css';
+import {Text} from '../../../../../UI/Text';
+import PropTypes from 'prop-types';
+
+export const Content = ({title, author}) => (
+  <div className={style.content}>
+    <Text As="h2" className={style.title}>
+      <Text
+        As="a"
+        size={18}
+        tsize={24}
+        className={style.linkPost}
+        href="#post"
+      >
+        {title}
+      </Text>
+    </Text>
+    <Text
+      As="a"
+      size={12}
+      tsize={14}
+      color="orange"
+      className={style.linkAuthor}
+      href="#author"
+    >
+      {author}
+    </Text>
+  </div>
+);
+
+Content.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+};
--- src/components/Main/List/Post/Post.jsx
+++ src/components/Main/List/Post/Post.jsx
@@ -1,12 +1,10 @@
 import style from './Post.module.css';
-// import notphoto from './img/notphoto.jpg';
 import PropTypes from 'prop-types';
-// import formatDate from '../../../../utils/formDate';
 import Time from './Time/Time';
 import {Rating} from './Rating/Rating';
 import {UserPhoto} from './UserPhoto/UserPhoto';
-import {Text} from '../../../../UI/Text';
 import {ReactComponent as DeleteSvg} from './img/delete.svg';
+import {Content} from './Content/Content';
 
 export const Post = ({postData}) => {
   const {title, author, ups, date, thumbnail} = postData;
@@ -15,29 +13,7 @@ export const Post = ({postData}) => {
     <li className={style.post}>
       <UserPhoto title={title} thumbnail={thumbnail}/>
 
-      <div className={style.content}>
-        <Text As="h2" className={style.title}>
-          <Text
-            As="a"
-            size={18}
-            tsize={24}
-            className={style.linkPost}
-            href="#post"
-          >
-            {title}
-          </Text>
-        </Text>
-        <Text
-          As="a"
-          size={12}
-          tsize={14}
-          color="orange"
-          className={style.linkAuthor}
-          href="#author"
-        >
-          {author}
-        </Text>
-      </div>
+      <Content title={title} author={author}></Content>
 
       <Rating ups={ups} />
 
@@ -52,5 +28,4 @@ export const Post = ({postData}) => {
 
 Post.propTypes = {
   postData: PropTypes.object,
-  // postData: PropTypes.array,
 };
