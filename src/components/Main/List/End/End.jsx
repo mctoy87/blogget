8c0d497747c2670ba5313fbcc0618d275d9cba96
mctoy87 <mctoy87@mail.ru>
2024-02-26 06:45:10 +0200

    redactoring

---
+++ src/components/Main/List/End/End.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useRef} from 'react';
+import {postsRequestAsync} from '../../../../store/posts/postsAction';
+import {useDispatch} from 'react-redux';
+import style from './End.module.css';
+
+export const End = () => {
+  const endList = useRef(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // if (!endList.current) return;
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        dispatch(postsRequestAsync());
+      }
+    }, {
+      rootMargin: '100px',
+    });
+
+    endList.current && observer.observe(endList.current);
+
+    return () => {
+      if (endList.current) {
+        observer.unobserve(endList.current);
+      }
+    };
+  }, [endList.current]);
+
+  return (
+    <li ref={endList} className={style.end}/>
+  );
+};
--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,51 +1,24 @@
-import {useEffect, useRef} from 'react';
 import style from './List.module.css';
 import Post from './Post';
 import PropTypes from 'prop-types';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import {postsRequestAsync} from '../../../store/posts/postsAction';
-import {Outlet, useParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import Loader from '../../../UI/Loader';
-
+import {useEffect} from 'react';
+import {End} from './End/End';
 
 export const List = () => {
   const postsData = useSelector(state => state.posts.posts);
   const postsAfter = useSelector(state => state.posts.after);
-  console.log('postsData: ', postsData);
   const loading = useSelector(state => state.posts.loading);
-
-  const endList = useRef(null);
   const dispatch = useDispatch();
   const {page} = useParams();
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    dispatch(postsRequestAsync());
-  };
 
   useEffect(() => {
     dispatch(postsRequestAsync(page));
   }, [page]);
-
-  useEffect(() => {
-    if (!endList.current) return;
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        console.log('Обнаружен');
-        dispatch(postsRequestAsync());
-      }
-    }, {
-      rootMargin: '100px',
-    });
-
-    observer.observe(endList.current);
-
-    return () => {
-      if (endList.current) {
-        observer.unobserve(endList.current);
-      }
-    };
-  }, [endList.current]);
 
   return (
     <>
@@ -71,12 +44,12 @@ export const List = () => {
           (postsData && postsAfter && (postsData.length > 20)) ? (
             <button
               className={style.morePosts}
-              onClick={(e) => handleClick(e)}
+              onClick={(e) => dispatch(postsRequestAsync())}
             >
               Загрузить еще
             </button>
           ) : (
-            <li ref={endList} className={style.end}/>
+            <End />
           )
         }
       </ul>
