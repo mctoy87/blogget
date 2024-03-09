92b55789ef888effe39dae3d78242a4978090e13
mctoy87 <mctoy87@mail.ru>
2024-03-09 13:57:27 +0200

    fix const REDIRECT_URL

--- src/api/const.js
+++ src/api/const.js
@@ -3,6 +3,6 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'sgRuUgDwvrsPMFOKj_Hl7w';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-umber.vercel.app/auth';
 export const SCOPE = 'identity submit read';
 
